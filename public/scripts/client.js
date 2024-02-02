ea532cde36411cb82891d74b2e879bfb89fb0ade
Shadrak Lagadi <shadrl19@gmail.com>
2024-02-02 14:30:12 -0700

    Add functions to prepend new tweets to page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(() => {
+  
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    let tweetsContainer = $("#tweets-container").html('');
+    for (let i = 0; i < tweets.length; i++) {
+      const tweet = tweets[i];
+      let storedTweet = createTweetElement(tweet);
+      console.log(storedTweet)
+      tweetsContainer.prepend(storedTweet);
+    }
+}
+
+
+
+  const createTweetElement = function (obj) {
+
+    const markup = `
+    <div class="tweet-header" id="user-info">
+    <div id="user-real-name">
+    <img src="${obj.user.avatars}">
+    ${obj.user.name}
+    </div>
+    <div id="username">${obj.user.handle}</div>
+  </div>
+  <p class="tweet-content">${obj.content.text}</p>
+  <hr>
+  <div class="tweet-footer">
+    <div class="tweet-footer-date">${obj.created_at}</div>
+    <div class="tweet-footer-icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </div>
+  `
+    const $tweet = $(`<article class="tweet">${markup}</article>`);
+  
+    return $tweet;
+  }
+  
+ renderTweets(data);
+
+})
