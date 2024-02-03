ae881caaeb0609c6a160855f8b8e27e2740f4fd3
Shadrak Lagadi <shadrl19@gmail.com>
2024-02-02 23:02:23 -0700

    Add loadTweet function and use Ajax GET request. Add comments. Lint.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,93 +5,83 @@
  */
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
+// Make sure the page loads first
 $(document).ready(() => {
 
-  
+  // Handle post request for tweet-text form
   $('#tweet-form').on('submit', (event) => {
     event.preventDefault();
     let text = $('#tweet-text').serialize();
-    console.log(text);
-    
 
     $.ajax({
       url: 'http://localhost:8080/tweets',
       type: 'POST',
       data: { text },
       success: (res) => {
-        console.log(res);
       },
       error: (error) => {
-        console.log('There was an Error', error)
+        console.log('There was an Error', error);
       }
     });
   });
 
-
+  // Responsible for fetching tweets from the http://localhost:8080/tweets data page
+  const loadTweets = function() {
   
-  const renderTweets = function(tweets) {
-    // loops through tweets
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      type: 'GET',
+      success: (tweets) => {
+        // If successful, render the page
+        renderTweets(tweets);
+      },
+      error: (error) => {
+        console.log('Error loading tweets', error);
+      }
+    });
+  };
+  // Function call
+  loadTweets();
+  
+  // Iterate through the http://localhost:8080/tweets array data
+  const renderTweets = function(arr) {
+
     let tweetsContainer = $("#tweets-container").html('');
-    for (let i = 0; i < tweets.length; i++) {
-      const tweet = tweets[i];
+    for (let i = 0; i < arr.length; i++) {
+      const tweet = arr[i];
+      // Convert data into the mark up that is created in the createTweetElement function
       let storedTweet = createTweetElement(tweet);
+
       tweetsContainer.prepend(storedTweet);
     }
-}
+  };
 
 
-
-  const createTweetElement = function (obj) {
-
-    const markup = `
-    <div class="tweet-header" id="user-info">
-    <div id="user-real-name">
-    <img src="${obj.user.avatars}">
-    ${obj.user.name}
-    </div>
+  // Create markup article to show in new-tweet section
+  const createTweetElement = function(obj) {
+  
+    let markUp = `
+  <div class="tweet-header" id="user-info">
+     <div id="user-real-name">
+      <img src="${obj.user.avatars}">
+       ${obj.user.name}
+     </div>
     <div id="username">${obj.user.handle}</div>
   </div>
-  <p class="tweet-content">${obj.content.text}</p>
-  <hr>
+    <p class="tweet-content">${obj.content.text}</p>
+     <hr>
   <div class="tweet-footer">
     <div class="tweet-footer-date">${obj.created_at}</div>
-    <div class="tweet-footer-icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
+     <div class="tweet-footer-icons">
+       <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
     </div>
   </div>
-  `
-    const $tweet = $(`<article class="tweet">${markup}</article>`);
+  `;
+    const $tweet = $(`<article class="tweet">${markUp}</article>`);
   
     return $tweet;
-  }
-  
- renderTweets(data);
+  };
 
 });
